86598eaf3d060e6a43975dc74511da4f41fee106
Zac Allen <zac.allen@outlook.com>
2021-07-09 10:00:18 +0200

    changed to :class{active:}

--- app.js
+++ app.js
@@ -10,11 +10,11 @@ const app = Vue.createApp({
     methods: {
         boxSelected(box) {
             if (box === 'A'){
-                this.boxASelected = true;
-            } else if (box === 'A'){
-                this.boxBSelected = true;
+                this.boxASelected = !this.boxASelected;
+            } else if (box === 'B'){
+                this.boxBSelected = !this.boxBSelected;
             } else if (box === 'C'){
-                this.boxCSelected = true;
+                this.boxCSelected = !this.boxCSelected;
             }
         }
     }
